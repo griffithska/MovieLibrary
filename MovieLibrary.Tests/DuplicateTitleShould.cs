6693ead387df99a8b44c4db561fce290237c7671
Karl Griffiths <karl.g.griffiths@gmail.com>
2021-09-20 22:42:30 -0500

    Added unit tests

---
+++ MovieLibrary.Tests/DuplicateTitleShould.cs
@@ -0,0 +1,34 @@
+using System;
+using Xunit;
+using MovieLibrary;
+
+namespace MovieLibrary.Tests
+{
+    public class MovieLibrary_DuplicateTitleShould
+    {
+        [Fact]
+        public void DuplicateTitle_InputToyStory1995_ReturnTrue() //Unique Title?
+        {
+            var file = "C:/Git/MovieLibrary/MovieLibrary/data/movies-short.csv";
+            var movieManager = new MovieManager();
+            movieManager.Movies = CsvFile.LoadFile(file);
+            
+            bool result = movieManager.DuplicateTitle("Toy Story (1995)");
+
+            Assert.True(result, "'Toy Story (1995)' is a duplicate.");
+        }
+
+        [Fact]
+        public void DuplicateTitle_InputFakeTitle_ReturnFalse() //Unique Title?
+        {
+            var file = "C:/Git/MovieLibrary/MovieLibrary/data/movies-short.csv";
+            var movieManager = new MovieManager();
+            movieManager.Movies = CsvFile.LoadFile(file);
+            
+            bool result = movieManager.DuplicateTitle("Fake Title");
+
+            Assert.False(result, "'Fake Title' is a not duplicate.");
+        }
+
+    }
+}
